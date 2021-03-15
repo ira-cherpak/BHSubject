0256b7282cbcdc87830eb9d9615a13bab26b885a
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-03-15 18:22:54 +0200

    Added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {pipe, Subject} from "rxjs";
-import {filter} from "rxjs/operators";
+import {pipe, Subject} from 'rxjs';
+import {debounceTime, filter} from 'rxjs/operators';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,29 @@ export class AppComponent implements OnInit {
   clickSubject$ = new Subject();
   title = 'no';
   counter = 0;
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      textInput: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.clickSubject$?.pipe(filter(i => i === 'increaseMeMain'))
       .subscribe(() => ++this.counter);
+    console.log(this.form.valueChanges);
+    this.form.valueChanges
+      .pipe(debounceTime(2000))
+      .subscribe(val => {
+        console.log(123);
+      });
+
+
   }
 
   incr() {
-    this.clickSubject$.next('increaseMeChild')
+    this.clickSubject$.next('increaseMeChild');
   }
 }
 
